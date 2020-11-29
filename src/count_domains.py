704c5af719282c881fd529a95b68ed5ce93ae266
Lawrence Lai <laitcl@gmail.com>
2020-11-28 20:09:33 -0800

    Create table queries added

--- src/count_domains.py
+++ src/count_domains.py
@@ -7,6 +7,7 @@ from io import StringIO
 from warcio.archiveiterator import ArchiveIterator
 from collections import Counter, defaultdict
 from urllib.parse import unquote, urlparse
+import psycopg2
 
 INSTAGRAM_LINK_PATTERN = re.compile('')
 
@@ -14,11 +15,12 @@ project_dir = Path(os.path.dirname(os.path.realpath(__file__))).parent
 
 warc_archive = str(project_dir) + "/tmp/CC-MAIN-20201101001251-20201101031251-00719.warc.gz"
 
-account_count=Counter()
-linked_instagrams = defaultdict(list)
+address_count=Counter()
+address_linked = defaultdict(list)
 
 domain = "instagram.com"
 proper_domain = "https://www.instagram.com/"
+
 with open(warc_archive, 'rb') as stream:
 	for record in ArchiveIterator(stream):
 		if record.rec_type == 'response':
@@ -32,6 +34,43 @@ with open(warc_archive, 'rb') as stream:
 							path = urlparse(href).path
 							instagram_link = proper_domain+path
 							account_count[instagram_link]+=1 
-							linked_instagrams[instagram_link].append(record.rec_headers.get_header('WARC-TARGET-URI'))
-							print(account_count)
-							print(linked_instagrams)
+							address_linked[instagram_link].append(record.rec_headers.get_header('WARC-TARGET-URI'))
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="cc_insta",
+    user="postgres",
+    password="")
+
+cur = conn.cursor()
+
+create_instagram_links_sql="""
+CREATE TABLE IF NOT EXISTS instagram_links
+(
+id              serial PRIMARY KEY,
+insta_link      text
+);
+"""
+
+create_address_linked_count_sql="""
+CREATE TABLE IF NOT EXISTS address_linked_count 
+(
+id                  serial PRIMARY KEY, 
+instagram_link_id   integer REFERENCES instagram_links,
+count               int
+)
+;
+"""
+
+create_address_linked_by_sql="""
+CREATE TABLE IF NOT EXISTS address_linked_by 
+(
+id                  serial PRIMARY KEY,
+instagram_link_id   integer REFERENCES instagram_links ,
+referened_by        text
+);
+"""
+
+cur.execute(create_instagram_links_sql)
+cur.execute(create_address_linked_count_sql)
+cur.execute(create_address_linked_by_sql)
